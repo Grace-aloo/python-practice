0e0773ff67b6934e70ad5188d07cef135b3cd1cf
grace_aloo <graceaaloo@gmail.com>
2023-07-27 13:22:43 +0300

    practicing py

--- new.py
+++ new.py
@@ -2,3 +2,21 @@ i = 0
 while i < 5:
     print(i)
     i += 1
+
+secret = "ricky"
+guess = ""
+count = 0
+limit = 3
+out_of_guesses = False
+
+while guess != secret and not(out_of_guesses):
+    if count <limit:
+        guess = input("Enter guess: ")
+        count += 1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("Out of guesses, You lose")
+else:
+    print("You win")
